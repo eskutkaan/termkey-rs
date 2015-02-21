c9f73126afbca3b12bf30dc5b3eebc9adf7e6970
Mathias Hällman <github@mths.se>
2015-02-21 13:44:36 +0100

    Start moving away from old_io

--- build.rs
+++ build.rs
@@ -1,11 +1,10 @@
  #![feature(env)]
+ #![feature(process)]
 
-use std::old_io::Command;
-use std::old_io::process::InheritFd;
+use std::process::Command;
 
 fn main() {
     assert!(Command::new("./build_termkey.sh").
-            stdout(InheritFd(1)).stderr(InheritFd(2)).
             status().unwrap().success());
     println!("cargo:rustc-flags= -L {}/termkey-c/.libs -l termkey",
       std::env::var("CARGO_MANIFEST_DIR").unwrap());
